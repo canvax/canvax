68e4307205c4ecff496d0d8dbbb3b6190d279fce
litao.lt <litao.lt@alibaba-inc.com>
2019-11-13 22:10:14 +0800

    add rollup to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
- 
-gulp.task('default', () =>
-    gulp.src('src/**/*.js')
-        .pipe(babel({
-            presets: ['@babel/env'],
-            plugins: ["transform-es2015-modules-umd"]
-        }))
-        .pipe(gulp.dest('dist'))
-);
+const rollup = require('rollup');
+
+let babelSrc = ()=>{
+    return gulp.src('src/**/*.js')
+    .pipe(babel({
+        presets: ['@babel/env'],
+        plugins: ["transform-es2015-modules-umd"]
+    }))
+    .pipe(gulp.dest('dist')); 
+};
+
+let rollupDist = ()=>{
+    return rollup.rollup({
+        entry : './dist/index.js'
+    }).then( bundle => {
+        bundle.write({
+            format: "umd",
+            moduleName: "canvax",
+            dest: "./dist/index_umd.js"
+        });
+    } );
+};
+
+gulp.series( babelSrc, rollupDist );
+
